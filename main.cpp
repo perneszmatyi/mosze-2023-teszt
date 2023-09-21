2cc76ab2d09c54a085e7a9a4792a215f998ed7e3
perneszmatyi <59794751+perneszmatyi@users.noreply.github.com>
2023-09-21 14:08:14 +0100

    hibák commentelése

--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //N_ELEMENTS
+    int k  = 5
+    std::cout << '1-100 ertekek duplazasa' //; hiányzik
+    for (int i = 0;) //i; i++ hiányzik
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //; hiányzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) //, helyett ;
     {
-        atlag += b[i]
+        atlag += b[i]              //; hiányzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
